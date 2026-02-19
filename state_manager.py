0eb2ec69dbccbd46191d313d34a96961690076b5
tamelaos <tamelaos@student.21-school.ru>
2026-02-19 22:46:46 +0500

    beta

--- state_manager.py
+++ state_manager.py
@@ -1,8 +1,7 @@
 from typing import Optional
 from os import getenv
-from dotenv import dotenv_values
 
-async def store_state(redis_client, state: str, next_url: str, ttl: int = config.get("STATE_TTL", "")):
+async def store_state(redis_client, state: str, next_url: str, ttl: int = int(getenv("STATE_TTL", 0))):
     key = f"{getenv("STATE_PREFIX")}{state}"
     await redis_client.setex(key, ttl, next_url)
 
