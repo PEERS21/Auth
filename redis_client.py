16d8e0d429450ec1e3ace2840ca26829d79db8a6
tamelaos <tamelaos@student.21-school.ru>
2026-02-19 23:30:09 +0500

    beta

--- redis_client.py
+++ redis_client.py
@@ -1,14 +1,10 @@
 import redis.asyncio as redis
+from os import getenv
 
 async def init_redis(app):
-    app['redis'] = redis.Redis(
-        host='redis',
-        port=6379,
-        db=0,
-        decode_responses=True,
-        encoding='utf-8'
-    )
+    app['pool'] = redis.ConnectionPool.from_url(getenv("REDIS_URL"))
+    app['redis'] = redis.Redis(connection_pool=app['pool'])
 
 async def close_redis(app):
     if 'redis' in app:
-        await app['redis'].close()
+        await app['redis'].aclose()
