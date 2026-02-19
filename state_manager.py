dbb7ce48c17032653aeae0ca2b1bc9b752dcf34e
KaraGOD <61622699+AlgoApi@users.noreply.github.com>
2026-02-19 18:12:09 +0500

    Update state_manager.py

--- state_manager.py
+++ state_manager.py
@@ -1,13 +1,12 @@
 from typing import Optional
-
+from os import getenv
 from dotenv import dotenv_values
-config = dotenv_values("/run/secrets/peers_auth")
 
 async def store_state(redis_client, state: str, next_url: str, ttl: int = config.get("STATE_TTL", "")):
-    key = f"{config.get("STATE_PREFIX", "")}{state}"
+    key = f"{getenv("STATE_PREFIX")}{state}"
     await redis_client.setex(key, ttl, next_url)
 
 
 async def pop_state(redis_client, state: str) -> Optional[str]:
-    key = f"{config.get("STATE_PREFIX", "")}{state}"
+    key = f"{getenv("STATE_PREFIX")}{state}"
     return await redis_client.getdel(key)
